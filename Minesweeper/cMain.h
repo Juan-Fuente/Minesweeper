3e239cecb86d9ee2d78fe6e5b108eeead086ef51
JuanFuente <lemoniggy@gmail.com>
2022-07-06 21:25:14 -0400

    replaced GUI with 10x10 butons and grid sizable now

--- Minesweeper/cMain.h
+++ Minesweeper/cMain.h
@@ -8,9 +8,14 @@ public:
 		~cMain();
 
 public:
-	wxButton* m_btn1 = nullptr;
-	wxTextCtrl* m_txt1 = nullptr;
-	wxListBox* m_list1 = nullptr;
+	
+	int nFieldWidth = 10;
+	int nFieldHeight = 10;
+	wxButton **btn;
+	
+	//wxButton* m_btn1 = nullptr;
+	//wxTextCtrl* m_txt1 = nullptr;
+	//wxListBox* m_list1 = nullptr;
 
 	void OnButtonClicked(wxCommandEvent& evt);
 
